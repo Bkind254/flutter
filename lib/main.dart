c708049b9428b05a8a83fae12fac04211f9008b1
Bkind254 <langatbrian02@gmail.com>
2023-11-08 13:59:34 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,13 +9,76 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //Functions and methods
+  void userTapped() {
+    print("User tapped");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("My App Bar"),
+            backgroundColor: Colors.deepPurple,
+            elevation: 0,
+            leading: Icon(Icons.menu),
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+          ),
+
+          /*body: SingleChildScrollView(*/
+          body: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              //1st box
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.deepPurple,
+              ),
+
+              //2nd box
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.deepPurple[400],
+              ),
+
+              //3rd box
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.deepPurple[200],
+                child: GestureDetector(
+                  onTap: userTapped,
+                  child: Center(child: Text("Tap me!")),
+                ),
+              ),
+            ],
+          ),
+        )
+
+        /* Center(
+            child: Container(
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(20)),
+          padding: EdgeInsets.all(25),
+          /*child: Text(
+            "Mitch KOKO",
+            style: TextStyle(
+                color: Colors.white, fontSize: 28, fontWeight: FontWeight.bold),
+          ),*/
+          child: Icon(
+            Icons.star,
+            color: Colors.white,
+            size: 64,
+          ),
+        )),*/
+        // ),
+        );
   }
 }
